fcdc98eaa4425752047cb1b999e3b614b1faa610
michielvha <michielvh@outlook.com>
2026-02-16 14:50:54 +0100

    chore: fix formatting

--- proxmox/module/outputs.tf
+++ proxmox/module/outputs.tf
@@ -6,7 +6,9 @@ output "proxmox_version" {
 output "iso_file_path" {
   description = "Path to the downloaded Debian netinst ISO"
   value       = proxmox_virtual_environment_download_file.test_iso.id
-}output "freebsd_iso_file_path" {
+}
+
+output "freebsd_iso_file_path" {
   description = "Path to the downloaded FreeBSD amd64 ISO (checksum-verified)"
   value       = proxmox_virtual_environment_download_file.freebsd_iso.id
 }
